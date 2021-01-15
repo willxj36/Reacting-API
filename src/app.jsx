7221798d1fc5b3ae747eecf4fda0950c67a9be6d
willxj36 <willxj36@gmail.com>
2021-01-15 16:31:20 -0700

    phase 1 done

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import Cards from './components/cards';
+
 
 class App extends Component {
+    state = {}
 
+    render() {
+        return(
+            <>
+                <div className="jumbotron jumbotron-fluid bg-dark">
+                    <div className="container">
+                        <h1 className="display-4 text-white">Studio Ghibli Film Portfolio</h1>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <Cards />
+                    </div>
+                </div>
+            </>
+        )
+    }
 }
 
 export default App;
