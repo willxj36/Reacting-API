6ba5f450c1656da3151b252f28a5cd16e3bc2b05
willxj36 <willxj36@gmail.com>
2021-01-15 17:33:21 -0700

    finished phase 2

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import Cards from './components/cards';
+import LoadButton from './components/loadbutton';
 
 
 class App extends Component {
-    state = {}
+    state = { loadCmd: false }
+
+    updateLoad = (loadState) => {
+        this.setState({ loadCmd: loadState });
+    }
 
     render() {
         return(
@@ -13,9 +18,12 @@ class App extends Component {
                         <h1 className="display-4 text-white">Studio Ghibli Film Portfolio</h1>
                     </div>
                 </div>
+                <div className="container bg-muted">
+                    <LoadButton loadCmd={this.state.loadCmd} updateLoad={this.updateLoad} />
+                </div>
                 <div className="container">
                     <div className="row">
-                        <Cards />
+                        <Cards loadCmd={this.state.loadCmd} />
                     </div>
                 </div>
             </>
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -4,35 +4,41 @@ import 'es6-promise';
 
 class Cards extends Component {
     state = {
-        componentLoaded: false,
-        filmArray: []
+        componentLoaded: false, //placeholder, might not be used
+        filmArray: [],
     }
 
-    componentDidMount() {
-        let newFilm;
-        this.setState({ componentLoaded: true });
-        fetch("https://ghibliapi.herokuapp.com/films")
-        .then(res => res.json())
-        .then(obj => {
-            newFilm = obj.map((film) => {
-                return(
-                    <div className="card col-5 m-2 shadow border rounded" key={film.id}>
-                        <div className="card-body">
-                            <h4 className="card-title">{film.title}</h4>
-                            <h6 className="card-subtitle m-2 text-muted">{film.release_date}</h6>
-                            <p className="card-text">{film.description}</p>
+    componentDidUpdate = (prevProps) => {
+        if(this.props.loadCmd !== prevProps.loadCmd) {
+            let newFilm;
+            this.setState({ componentLoaded: true });
+            fetch("https://ghibliapi.herokuapp.com/films")
+            .then(res => res.json())
+            .then(obj => {
+                newFilm = obj.map((film) => {
+                    return(
+                        <div className="card col-md-5 m-2 shadow border rounded" key={film.id}>
+                            <div className="card-body">
+                                <h4 className="card-title">{film.title}</h4>
+                                <h6 className="card-subtitle m-2 text-muted">{film.release_date}</h6>
+                                <p className="card-text">{film.description}</p>
+                            </div>
                         </div>
-                    </div>
-                )
+                    )
+                })
+                this.setState({ filmArray: [...this.state.filmArray, ...newFilm] });
             })
-            this.setState({ filmArray: [...this.state.filmArray, ...newFilm] });
-        })
-        .catch(err => console.log(err));
+            .catch(err => console.log(err));
+        }
     }
 
     render() {
-        return ( <> {this.state.filmArray} </>);
+        // if(this.props.loadCmd) {
+            return(<> {this.state.filmArray} </>);
+        // } else {
+        //     return null;
+        // } 
     }
 }
 
-export default Cards
+export default Cards;
